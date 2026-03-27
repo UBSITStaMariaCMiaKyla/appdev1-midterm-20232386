60789c870b3513fba48e9ac360916f35587a9e06
UBSITStaMariaCMiaKyla <20232386@s.ubaguio.edu>
2026-03-27 11:28:52 +0800

    Initialize midterm examination project with Angular and Bootstrap

---
+++ src/app/components/task-info/task-info.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-info',
+  imports: [],
+  templateUrl: './task-info.html',
+  styleUrl: './task-info.css',
+})
+export class TaskInfo {}
