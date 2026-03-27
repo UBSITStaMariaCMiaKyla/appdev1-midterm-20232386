a2c8080c28d9e0ff89cc1b8383b11878754374fa
UBSITStaMariaCMiaKyla <20232386@s.ubaguio.edu>
2026-03-27 12:19:08 +0800

    Fix all import paths to match generated file names

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { TaskListComponent } from './components/task-list/task-list.component';
-import { TaskDetailComponent } from './components/task-detail/task-detail.component';
-import { TaskAddComponent } from './components/task-add/task-add.component';
-import { TaskInfoComponent } from './components/task-info/task-info.component';
-import { TaskEditComponent } from './components/task-edit/task-edit.component';
+import { TaskListComponent } from './components/task-list/task-list';
+import { TaskDetailComponent } from './components/task-detail/task-detail';
+import { TaskAddComponent } from './components/task-add/task-add';
+import { TaskInfoComponent } from './components/task-info/task-info';
+import { TaskEditComponent } from './components/task-edit/task-edit';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/tasks', pathMatch: 'full' },
