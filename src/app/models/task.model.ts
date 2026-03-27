ee36654f5f334df0a9343e319ed398246ff5c844
UBSITStaMariaCMiaKyla <20232386@s.ubaguio.edu>
2026-03-27 11:39:55 +0800

    Add task model and service

---
+++ src/app/models/task.model.ts
@@ -0,0 +1,9 @@
+export interface Task {
+    id: number;
+    title: string;
+    description: string;
+    dueDate: string;
+    status: 'Pending' | 'In Progress' | 'Completed';
+    priority: 'Low' | 'Medium' | 'High';
+    createdAt: string;
+  }
